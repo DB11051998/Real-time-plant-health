a919ace5a5a7a556fcc3fe2161310e72b95cf25a
Debasish Behera <debasish.notify@gmail.com>
2020-05-10 20:18:32 +0530

    commit

---
+++ arduino_soil.ino
@@ -0,0 +1,33 @@
+#include <SoftwareSerial.h>
+#include <ArduinoJson.h>
+SoftwareSerial s(3,5);
+ 
+void setup() {
+  s.begin(9600);
+  pinMode(A1,INPUT);
+  pinMode(A2,OUTPUT);
+  
+}
+StaticJsonBuffer<1000> jsonBuffer;
+JsonObject& root = jsonBuffer.createObject();
+void loop() {
+  root["gasv"]= analogRead(A1);
+  //root["gasv"] = (100-(double(root["gasv"])/1023.00)*100);
+  if(root["gasv"]>=800){
+    digitalWrite(A2,LOW);
+  }
+  else{
+    digitalWrite(A2,HIGH);
+  }
+  //digitalWrite(A2,HIGH);
+  //delay(2000);
+  //digitalWrite(A2,LOW);
+  //delay(2000);
+  
+  
+ 
+if(s.available()>0)
+{
+ root.printTo(s);
+}
+}
---
+++ new_node_mcu_new.ino
@@ -0,0 +1,57 @@
+#include <ESP8266WiFi.h>  
+#include <WiFiClient.h>  
+#include <ThingSpeak.h>  
+#include <SoftwareSerial.h>
+SoftwareSerial s(D5,D3);
+#include <ArduinoJson.h>  
+const char* ssid = "sonu";  
+const char* password = "sonu11051998";  
+WiFiClient client;  
+unsigned long myChannelNumber =749889;  
+const char * myWriteAPIKey = "Z95TLN5OGT8W05LE";  
+ 
+void setup()  
+{  
+  Serial.begin(9600);  
+  s.begin(9600);
+  while (!Serial) continue;
+  delay(10);  
+  // Connect to WiFi network  
+  Serial.println();  
+  Serial.println();  
+  Serial.print("Connecting to ");  
+  Serial.println(ssid);  
+  WiFi.begin(ssid, password);  
+  while (WiFi.status() != WL_CONNECTED)  
+  {  
+   delay(500);  
+   Serial.print(".");  
+  }
+  Serial.println("");  
+  Serial.println("WiFi connected");  
+  // Print the IP address  
+  Serial.println(WiFi.localIP());  
+  ThingSpeak.begin(client);  
+}  
+void loop()   
+{  
+  static boolean data_state = false;  
+  StaticJsonBuffer<1000> jsonBuffer;
+  JsonObject& root = jsonBuffer.parseObject(s);
+ 
+  if (root == JsonObject::invalid())
+  {
+    return;
+  }
+  //Print the data in the serial monitor
+  Serial.println("JSON received and parsed");
+  root.prettyPrintTo(Serial);
+  Serial.println("");
+  int data4=root["gasv"];
+  Serial.println(data4);
+  Serial.println("");
+  Serial.println("---------------------xxxxx--------------------");
+  Serial.println("");  
+  ThingSpeak.writeField(myChannelNumber,1,data4, myWriteAPIKey);   
+  delay(30000); // ThingSpeak will only accept updates every 15 seconds.  
+} 
